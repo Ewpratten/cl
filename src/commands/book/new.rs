a0b9327e489491ae9975803f3baf09877aa8a629
Evan Pratten <ewpratten@gmail.com>
2021-06-14 09:49:24 -0400

    logbook creation

--- src/commands/book/new.rs
+++ src/commands/book/new.rs
@@ -1,3 +1,7 @@
+use colored::Colorize;
+
+use crate::lib::{data::logbook::Logbook, ensure_storage_location_exists, get_data_books_path, get_data_default_book_path};
+
 pub fn exec_new_book(
     name: &str,
     grid: Option<&str>,
@@ -6,5 +10,43 @@ pub fn exec_new_book(
     default: bool,
     import: Option<&str>,
 ) {
-    todo!();
+    // Ensure we have our directories
+    ensure_storage_location_exists();
+
+    // If the book already exists, error out
+    let mut book_path = get_data_books_path();
+    book_path.push(format!("{}.json", name));
+    if std::fs::metadata(&book_path).is_ok() {
+        println!("{}", "Logbook already exists!".bright_red());
+        println!("Did you mean to use the \"edit\" command?");
+        return;
+    }
+
+    // Create a logbook
+    let mut book = Logbook {
+        name: name.to_string(),
+        grid: grid.map(Into::into),
+        callsign: callsign.map(Into::into),
+        description: description.map(Into::into),
+        entries: Vec::new(),
+    };
+
+    // Handle import
+    if let Some(import_path) = import {
+        panic!("Importing ADIF files is not yet supported");
+    }
+
+    // Save the logbook
+    println!("{:?}", book_path);
+    autojson::jsonify(&book, &book_path).expect("Failed to serialize logbook");
+
+    // Handle saving this as the default
+    if default {
+        std::fs::write(get_data_default_book_path(), name)
+            .expect("Failed to set book as default");
+        println!("Set book as default");
+    }
+
+    // Log success
+    println!("New logbook created");
 }
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,1 +1,29 @@
+use std::path::PathBuf;
+
 pub mod data;
+
+
+pub fn get_data_root_path() -> PathBuf {
+    let mut out = dirs::home_dir().expect("Could not find user home directory!");
+    out.push(".cl.data");
+    out
+}
+
+pub fn get_data_books_path() -> PathBuf {
+    let mut out = get_data_root_path();
+    out.push("books");
+    out
+}
+
+pub fn get_data_default_book_path() -> PathBuf {
+    let mut out = get_data_root_path();
+    out.push("default-book");
+    out
+}
+
+pub fn ensure_storage_location_exists() {
+    if !std::fs::metadata(get_data_books_path()).is_ok() {
+        std::fs::create_dir_all(get_data_books_path())
+            .expect("Failed to create logbook directory");
+    }
+}
